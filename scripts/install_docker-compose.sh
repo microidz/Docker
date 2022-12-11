f00142e84534c045b294c01f2d5e50b2eb2f19da
microidz <michel@cusin.ca>
2022-12-11 12:42:24 -0500

    First Commit

---
+++ scripts/install_docker-compose.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+### Docker Installer
+### Source: https://docs.docker.com/engine/install/ubuntu/
+
+###  Update the apt package index and install packages to allow apt to use a repository over HTTPS:
+
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+### Add Docker's official GPG key:
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+### Set up the repository:
+### Update the apt package index
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+### Install Docker Engine
+
+sudo apt-get update
+
+### Install docker and docker compose on Ubuntu
+
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
